c8fb22bb8c98d7fd2d36e673148d445af9f98b9d
uo277553 <uo277553@uniovi.es>
2022-03-07 20:27:20 +0100

    Creacion del productController y router, sin repositorio

---
+++ restapi/routers/ProductRouter.ts
@@ -0,0 +1,43 @@
+import express, { Request, Response, Router } from 'express';
+import {check} from 'express-validator';
+import * as productController from "./../controladores/ProductController";
+
+const routerProduct:Router = express.Router();
+
+//routers
+routerProduct.get(
+    "/list",
+    productController.findAllProducts
+);
+
+routerProduct.get(
+  "/find/:id",
+  productController.findAllProducts
+);
+
+routerProduct.delete(
+  "/delete/:id",
+  productController.findAllProducts
+);
+
+routerProduct.post(
+  "/update/:id",[
+    //validaciones controlador
+    TODO
+    check('name').isLength({ min: 1 }).trim().escape(),
+    check('email').isEmail().normalizeEmail(),
+  ],
+  productController.findAllProducts
+);
+
+routerProduct.post(
+  "/add",[
+    //validaciones controlador
+    TODO
+    check('name').isLength({ min: 1 }).trim().escape(),
+    check('email').isEmail().normalizeEmail(),
+  ],
+  productController.addProduct
+);
+
+export default routerProduct;
