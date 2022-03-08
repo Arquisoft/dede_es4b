516df86a2c54fcf4b0b52e47ddffdcb64ac80c13
UO276208 <73529340+UO276208@users.noreply.github.com>
2022-03-08 19:04:23 +0100

    Esquema usuario, cambio a esquema de producto y conexión como función

--- restapi/models/product.ts
+++ restapi/models/product.ts
@@ -3,12 +3,14 @@ const {Schema, model} = require('mongoose')
 interface Product {
     name:string;
     section:string;
+    description:string;
     price:number;    
 }
 
 const productSchema = new Schema({
     name: {type:String, required: true},
     section: {type:String, required: true},
+    description: {type:String, required: true},
     price: {type:Number, required: true}
 });
 
---
+++ restapi/models/user.ts
@@ -0,0 +1,15 @@
+interface User {
+    name:string;
+    surname:string;
+    password:string;   
+}
+
+const userSchema = new Schema({
+    name: {type:String, required: true},
+    surname: {type:String, required: true},
+    password: {type:String, required: true},
+});
+
+const UserModel = model('User', userSchema);
+
+module.exports = UserModel;
