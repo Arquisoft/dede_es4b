148e0b88d9d101512f1454fda7bb574e143a9b5a
UO276208 <73529340+UO276208@users.noreply.github.com>
2022-03-09 18:51:21 +0100

    Union backlend bd

--- restapi/server.ts
+++ restapi/server.ts
@@ -4,6 +4,8 @@ import bp from 'body-parser';
 import promBundle from 'express-prom-bundle';
 import productRouter from "./routers/ProductRouter";
 
+const { dbConnection } = require('./mongo')
+
 const app: Application = express();
 const port: number = 5000;
 const options: cors.CorsOptions = {
@@ -15,6 +17,8 @@ const metricsMiddleware:RequestHandler = promBundle({includeMethod: true});
 app.use(metricsMiddleware);
 app.use(cors(options));
 app.use(bp.json());
+
+dbConnection();
 
 //use (añadir las nuevas url)
 app.use("/product", productRouter)
