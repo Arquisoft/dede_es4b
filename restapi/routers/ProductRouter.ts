c74cbaebec0b2b039c37490f91951582d977e4f8
uo277553 <uo277553@uniovi.es>
2022-03-09 19:59:08 +0100

    Arreglados error con los validadores

--- restapi/routers/ProductRouter.ts
+++ restapi/routers/ProductRouter.ts
@@ -1,6 +1,8 @@
 import express, { Router } from 'express';
 import {check} from 'express-validator';
 
+const {validateFields} = require("./../validadores/validador");
+
 const {
   addProduct,
   findAllProducts,
@@ -10,7 +12,6 @@ const {
 } = require("./../controladores/ProductController")
 
 const routerProduct:Router = express.Router();
-const {validador} = require("./../validadores/validador.js");
 
 //routers
 routerProduct.get(
@@ -27,19 +28,6 @@ routerProduct.delete(
   "/delete/:id",
   deleteProduct
 );
-/*
-routerProduct.put(
-  "/update/:id",[
-    check('name').isLength({ min: 1 }).trim().escape(),
-    check('section').isLength({ min: 1 }).trim().escape(),
-    check('description').isLength({ min: 1 }).trim().escape(),
-    check('price').isLength({ min: 1 }).trim().escape(),
-    check('price').isFloat(),
-    check('image').isLength({ min: 1 }).trim().escape(),
-    validador
-  ],
-  updateProduct
-);
 
 routerProduct.post(
   "/add",[
@@ -49,10 +37,22 @@ routerProduct.post(
     check('price').isLength({ min: 1 }).trim().escape(),
     check('price').isFloat(),
     check('image').isLength({ min: 1 }).trim().escape(),
-    validador
+    validateFields
   ],
   addProduct
 );
-*/
+
+routerProduct.put(
+  "/update/:id",[
+    check('name').isLength({ min: 1 }).trim().escape(),
+    check('section').isLength({ min: 1 }).trim().escape(),
+    check('description').isLength({ min: 1 }).trim().escape(),
+    check('price').isLength({ min: 1 }).trim().escape(),
+    check('price').isFloat(),
+    check('image').isLength({ min: 1 }).trim().escape(),
+    validateFields
+  ],
+  updateProduct
+);
 
 export default routerProduct;
