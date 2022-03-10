f595b437c4a13255e1dbab5b15c9702692ff7db2
uo271355 <uo271355@uniovi.es>
2022-03-10 11:31:23 +0100

    Primera versión layaout carrito

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -7,12 +7,15 @@ import Welcome from './components/Welcome';
 import UserList from './components/UserList';
 import  {getUsers} from './api/api';
 import {User} from './shared/shareddtypes';
+//import {Carrito} from './components';
+import Carrito from './components/Carrito/Carrito';
 import './App.css';
 
 function App(): JSX.Element {
 
   const [users,setUsers] = useState<User[]>([]);
-
+  const [carrito, setCarrito] = useState([]);
+  
   const refreshUserList = async () => {
     setUsers(await getUsers());
   }
@@ -22,15 +25,16 @@ function App(): JSX.Element {
   },[]);
 
   return (
-    <>
-      <Container maxWidth="sm">
-        <Welcome message="ASW students"/>
-        <Box component="div" sx={{ py: 2}}>This is a basic example of a React application using Typescript. You can add your email to the list filling the form below.</Box>
-        <EmailForm OnUserListChange={refreshUserList}/>        
-        <UserList users={users}/>
-        <Link href="https://github.com/pglez82/asw2122_0">Source code</Link>
-      </Container>
-    </>
+    //<>
+      //<Container maxWidth="sm">
+      //  <Welcome message="ASW students"/>
+      //  <Box component="div" sx={{ py: 2}}>This is a basic example of a React application using Typescript. You can add your email to the list filling the form below.</Box>
+      //  <EmailForm OnUserListChange={refreshUserList}/>        
+      //  <UserList users={users}/>
+      //  <Link href="https://github.com/pglez82/asw2122_0">Source code</Link>
+      //</Container>
+      <Carrito carrito={carrito}/>
+    //</>
   );
 }
 
