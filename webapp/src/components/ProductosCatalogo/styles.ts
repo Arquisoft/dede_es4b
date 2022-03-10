13afd999b82ee2d2dcbb8fd7b9e5261570a65ee7
UO276213 <uo276213@uniovi.es>
2022-03-10 17:10:16 +0100

    Arreglos menores en el diseño

--- webapp/src/components/ProductosCatalogo/styles.ts
+++ webapp/src/components/ProductosCatalogo/styles.ts
@@ -2,7 +2,6 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles(() => ({
     main: {
-        margin: 'auto',
-        maxWidth: '60%'
+        padding: '20% 15%',
     }
 }));
