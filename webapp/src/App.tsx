59f2ffc5f4b6e77101c762dba325eeb277b959c5
Miriam <uo277878@uniovi.es>
2022-03-14 21:20:25 +0100

    Barra de navegacion acabada

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -32,13 +32,7 @@ function App(): JSX.Element {
   return (
     <>
       <NavBar/>
-      <Container maxWidth="sm">
-        <Welcome message="ASW students"/>
-        <Box component="div" sx={{ py: 2}}>This is a basic example of a React application using Typescript. You can add your email to the list filling the form below.</Box>
-        <EmailForm OnUserListChange={refreshUserList}/>        
-        <UserList users={users}/>
-        <Link href="https://github.com/pglez82/asw2122_0">Source code</Link>
-      </Container>
+      
     </>
   );
 }
