03b946f15ed55cc63331d49aafd2fee812fd57ac
UO276213 <uo276213@uniovi.es>
2022-03-14 19:44:17 +0100

    -Añadido botón para ordenar productos del catálogo, no funcional -Añadida dependencia iconify Añadido botones de paginación Añadido título y otras mejoras visuales

--- webapp/src/components/ProductosCatalogo/ProductosCatalogo.tsx
+++ webapp/src/components/ProductosCatalogo/ProductosCatalogo.tsx
@@ -2,6 +2,9 @@ import React, {useState, useEffect} from 'react';
 import {Grid} from '@material-ui/core';
 import ProductoCatalogo from './ProductoCatalogo/ProductoCatalogo';
 import useStyles from './styles';
+import {Button, Divider, Menu, MenuItem, Pagination} from "@mui/material";
+import { Icon } from '@iconify/react';
+import Box from "@mui/material/Box";
 
 interface producto {
     ids: number;
@@ -32,9 +35,44 @@ const ProductosCatalogo = () => {
     }, [])
 
 
+    // Ordenar
+    const [open, setOpen] = useState(null);
+
+    const handleOpen = (event : any) => {
+        setOpen(event.currentTarget);
+    };
+
+    const handleClose = () => {
+       // TODO
+        setOpen(null)
+    };
+
     const classes = useStyles();
     return (
         <main className={classes.main}>
+            <div className={classes.titulo}>
+                <Box component="h1" color="indigo" >Catálogo de productos</Box>
+                <Divider />
+            </div>
+
+            <div className={classes.filtro}>
+                <Button onClick={handleOpen} disableRipple className={classes.paginacion}>
+                    Ordenar
+                    <Icon icon="ic:round-filter-list" />
+                </Button>
+            </div>
+
+            <Menu
+                keepMounted
+                anchorEl={open}
+                open={Boolean(open)}
+                onClose={handleClose}
+                anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+                transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+            >
+                <MenuItem onClick={handleClose}>Precio ascendente</MenuItem>
+                <MenuItem onClick={handleClose}>Precio descendente</MenuItem>
+            </Menu>
             <Grid container justifyContent="center" spacing={1}>
                 {productos.map(product => (
                     <Grid item key={product.ids} xs={12} sm={6} md={4} lg={3}>
@@ -42,6 +80,7 @@ const ProductosCatalogo = () => {
                     </Grid>
                 ))}
             </Grid>
+            <Pagination className={classes.paginacion} count={10} shape="rounded" />
         </main>
     );
 }
--- webapp/src/components/ProductosCatalogo/styles.ts
+++ webapp/src/components/ProductosCatalogo/styles.ts
@@ -2,6 +2,22 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles(() => ({
     main: {
-        padding: '20% 15%',
+        padding: '10% 15%',
+    },
+    titulo: {
+        display: "flex",
+        padding: '0% 0% 10%',
+        fontSize: "x-large",
+        justifyContent: "center",
+
+    },
+    filtro: {
+        display: "flex",
+        justifyContent: "right",
+    },
+    paginacion:  {
+        padding: "10% 0",
+        display: "flex",
+        justifyContent: "right",
     }
 }));
