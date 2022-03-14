e1b1e3d23c1f2fb1111da07fd46e599567e9dc5e
UO276213 <uo276213@uniovi.es>
2022-03-14 21:55:51 +0100

    Unido correctamente todo el frontEnd

--- webapp/src/components/ProductosCatalogo/ProductosCatalogo.tsx
+++ webapp/src/components/ProductosCatalogo/ProductosCatalogo.tsx
@@ -5,6 +5,7 @@ import useStyles from './styles';
 import {Button, Divider, Menu, MenuItem, Pagination} from "@mui/material";
 import { Icon } from '@iconify/react';
 import Box from "@mui/material/Box";
+import NavBar from "../AppBar/NavBar";
 
 interface producto {
     ids: number;
@@ -49,39 +50,42 @@ const ProductosCatalogo = () => {
 
     const classes = useStyles();
     return (
-        <main className={classes.main}>
-            <div className={classes.titulo}>
-                <Box component="h1" color="indigo" >Catálogo de productos</Box>
-                <Divider />
-            </div>
+        <>
+            <NavBar/>
+            <main className={classes.main}>
+                <div className={classes.titulo}>
+                    <Box component="h1" color="indigo" >Catálogo de productos</Box>
+                    <Divider />
+                </div>
 
-            <div className={classes.filtro}>
-                <Button onClick={handleOpen} disableRipple className={classes.paginacion}>
-                    Ordenar
-                    <Icon icon="ic:round-filter-list" />
-                </Button>
-            </div>
+                <div className={classes.filtro}>
+                    <Button onClick={handleOpen} disableRipple className={classes.paginacion}>
+                        Ordenar
+                        <Icon icon="ic:round-filter-list" />
+                    </Button>
+                </div>
 
-            <Menu
-                keepMounted
-                anchorEl={open}
-                open={Boolean(open)}
-                onClose={handleClose}
-                anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-                transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-            >
-                <MenuItem onClick={handleClose}>Precio ascendente</MenuItem>
-                <MenuItem onClick={handleClose}>Precio descendente</MenuItem>
-            </Menu>
-            <Grid container justifyContent="center" spacing={1}>
-                {productos.map(product => (
-                    <Grid item key={product.ids} xs={12} sm={6} md={4} lg={3}>
-                        <ProductoCatalogo product={product} />
-                    </Grid>
-                ))}
-            </Grid>
-            <Pagination className={classes.paginacion} count={10} shape="rounded" />
-        </main>
+                <Menu
+                    keepMounted
+                    anchorEl={open}
+                    open={Boolean(open)}
+                    onClose={handleClose}
+                    anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+                    transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+                >
+                    <MenuItem onClick={handleClose}>Precio ascendente</MenuItem>
+                    <MenuItem onClick={handleClose}>Precio descendente</MenuItem>
+                </Menu>
+                <Grid container justifyContent="center" spacing={1}>
+                    {productos.map(product => (
+                        <Grid item key={product.ids} xs={12} sm={6} md={4} lg={3}>
+                            <ProductoCatalogo product={product} />
+                        </Grid>
+                    ))}
+                </Grid>
+                <Pagination className={classes.paginacion} count={10} shape="rounded" />
+            </main>
+        </>
     );
 }
 
