19c98c693bcdf19cfb8abd7588122774b2814213
UO276213 <uo276213@uniovi.es>
2022-03-22 00:18:38 +0100

    Checkbox para mostrar la contraseña

--- webapp/src/pages/login.tsx
+++ webapp/src/pages/login.tsx
@@ -1,17 +1,30 @@
-import React from 'react'
+import React, {useState } from 'react'
 import {Button, Checkbox, FormControlLabel, TextField, Typography } from '@mui/material'
 import './login.css'
 
 const Login = () => {
+
+    /**
+     * Permite mostrar la contraseña
+     */
+    const [tipo, setTipo] = useState('password')
+
+    const cambiarVisibilidad = () => {
+        if (tipo === 'password')
+            setTipo("text")
+        else
+            setTipo("password")
+    }
+
     return (
         <div>
             <Typography  variant='h2' gutterBottom>Inicie sesión</Typography>
             <div style={{display: 'flex', flexDirection: 'column'}}>
                 <TextField label="Usuario" variant="outlined" margin="dense"></TextField>
-                <TextField type="password" label="Contraseña" variant="outlined" margin="dense"></TextField>
+                <TextField type={tipo} label="Contraseña" variant="outlined" margin="dense"></TextField>
                 <div style={{ display: 'flex', justifyContent: 'space-between', margin: '0.5em' }}>
-                    <FormControlLabel control={<Checkbox />} label="Recordar" />
-                    <Button variant="contained">Login</Button>  
+                    <FormControlLabel control={<Checkbox onChange={() => cambiarVisibilidad()}/>} label="Mostrar" />
+                    <Button variant="contained">Iniciar sesión</Button>  
                 </div>
             </div>
             <Typography style={{justifyContent: 'end'}}  variant='h6' gutterBottom>¿Sin cuenta? Regístrate <a href='/signup'>aquí</a></Typography>
