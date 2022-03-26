d45bd75fe2b5ab0200a77780e245e5f6651a2b47
UO276208 <73529340+UO276208@users.noreply.github.com>
2022-03-26 19:07:44 +0100

    Cambios en modelo (#105)
    * Modelo de datos actualizado
    
    Todos los archivos no relacionados es porque se han reescrito los commits realcionados con issue: https://github.com/Arquisoft/dede_es4b/issues/93

--- restapi/models/product.ts
+++ restapi/models/product.ts
@@ -2,18 +2,31 @@ const {Schema, model} = require('mongoose')
 
 interface Product {
     name:string;
-    section:string;
-    description:string;
     price:number;
-    image:string;    
+    short_description:string;
+    long_description:string;
+    brand:string;
+    category:string;
+    sub_category:string;
+    image:string; 
+
+    type:string;
+    color:string;
+    size:number;   
 }
 
 const productSchema = new Schema({
-    name: {type:String, required: [true, 'Name is mandatoy']},
-    section: {type:String, required: [true, 'Section is mandatoy']},
-    description: {type:String, required: true},
+    name: {type:String, required: true},
     price: {type:Number, required: true},
-    image: {type:String, required: true}
+    short_description: {type:String, required: true},
+    long_description: {type:String, required: true},
+    brand: {type:String, required: true},
+    category: {type:String, required: true},
+    sub_category: {type:String, required: true},
+    image: {type:String, required: true},
+    type: {type:String, required: false},
+    color: {type:String, required: false},
+    size: {type:Number, required: false},
 });
 
 const ProductModel = model('Product', productSchema);
