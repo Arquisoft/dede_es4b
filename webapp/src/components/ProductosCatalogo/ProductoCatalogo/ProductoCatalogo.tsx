f48200801b04ecbfbc36ffdce602aee1171ea915
UO276213 <uo276213@uniovi.es>
2022-03-26 23:50:29 +0100

    Prototipo de añadir productos del catalogo al carrito

--- webapp/src/components/ProductosCatalogo/ProductoCatalogo/ProductoCatalogo.tsx
+++ webapp/src/components/ProductosCatalogo/ProductoCatalogo/ProductoCatalogo.tsx
@@ -5,8 +5,24 @@ import { AddShoppingCart } from "@mui/icons-material";
 import { CardActionArea } from '@mui/material';
 import { producto } from '../../../pages/Catalogo';
 
+interface ProductoCarrito{
+    producto : producto;
+    cantidad : number;
+    precioTotal : number;
+}
+
+const carrito : ProductoCarrito[] = [];
+
+
 const ProductoCatalogo = ( {producto}: {producto: producto} ) => {
     const classes = useStyles();
+
+
+    const añadirAlCarrito = (producto : producto) => {
+        let productoCarrito : ProductoCarrito = {producto: producto, cantidad: 1, precioTotal: parseFloat(producto.price)};
+        carrito.push(productoCarrito);
+        console.log(carrito);
+    }
 
     return (
         <Card className={classes.root}>
@@ -26,7 +42,7 @@ const ProductoCatalogo = ( {producto}: {producto: producto} ) => {
             </CardActionArea>
             <CardActions className={classes.cardActions}>
                 <Typography > {producto.price} </Typography>
-                <IconButton aria-label="Add to Cart">
+                <IconButton aria-label="Add to Cart" onClick={() => añadirAlCarrito(producto)}>
                     <AddShoppingCart color='primary' fontSize='small' />
                 </IconButton>
             </CardActions>
