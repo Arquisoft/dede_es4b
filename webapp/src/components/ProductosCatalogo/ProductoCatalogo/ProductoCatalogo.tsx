4c48101bb90685709c66d78f0d2ba98ff2baafbd
UO276213 <uo276213@uniovi.es>
2022-03-26 23:02:22 +0100

    Correciones de código

--- webapp/src/components/ProductosCatalogo/ProductoCatalogo/ProductoCatalogo.tsx
+++ webapp/src/components/ProductosCatalogo/ProductoCatalogo/ProductoCatalogo.tsx
@@ -3,22 +3,23 @@ import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } fro
 import useStyles from './styles'
 import { AddShoppingCart } from "@mui/icons-material";
 import { CardActionArea } from '@mui/material';
+import { producto } from '../../../pages/Catalogo';
 
-const ProductoCatalogo = ({ producto: producto }) => {
+const ProductoCatalogo = ( {producto}: {producto: producto} ) => {
     const classes = useStyles();
 
     return (
         <Card className={classes.root}>
-            <CardActionArea href={'/' + producto.id}>
+            <CardActionArea href={'/' + producto.ids}>
                 <CardMedia className={classes.media} image={producto.image} title={producto.name} />
                 <CardContent>
                     <div className={classes.cardContent}>
-                        <Typography variant="h5" gutterButtom>
+                        <Typography variant="h5" gutterBottom>
                             {producto.name}
                         </Typography>
 
                         <div className={classes.description}>
-                            <Typography variant="p" color="textSecondary">{producto.description}</Typography>
+                            <Typography variant="subtitle1" color="textSecondary">{producto.description}</Typography>
                         </div>
                     </div>
                 </CardContent>
