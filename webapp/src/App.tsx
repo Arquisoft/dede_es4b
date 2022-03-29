9c62d21dffa1389717f0fe4d863cc695c87cdd83
UO276213 <uo276213@uniovi.es>
2022-03-29 23:34:55 +0200

    Al logearse se queda guardada la sesión

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,85 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
-import Container from '@mui/material/Container';
-import EmailForm from './components/EmailForm';
-import Welcome from './components/Welcome';
-import UserList from './components/UserList';
-
-import  {getUsers} from './api/api';
-import {User} from './shared/shareddtypes';
-import Carrito from './components/Carrito/Carrito';
-
-import NavBar from './components/AppBar/NavBar';
+import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Catalogo from "./pages/Catalogo";
 import Login from './pages/login';
+import Catalogo from './pages/Catalogo';
+
+export const isLogeado = (): boolean => {
+  return getToken() != null;
+}
+
+//TODO
+function getToken(): string | null {
+  const tokenString = sessionStorage.getItem('token');
+  // const userToken = JSON.parse(tokenString!);
+  // return userToken?.token;
+  return tokenString;
+}
 
 function App(): JSX.Element {
-
-  const [users,setUsers] = useState<User[]>([]);
-  const [productos, setProductos] = useState<Producto[]>([]);
-
-  interface Producto {
-
-    id: string;
-    name: string;
-    price: string;
-    image: string;
-
-  }
-
-  const refreshUserList = async () => {
-    setUsers(await getUsers());
-  }
-
-
-  useEffect(()=>{
-    refreshUserList();
-  },[]);
-
-  const getProductos = async () => {
-    let respuesta = await fetch('http://localhost:5000/product/list')
-
-    return respuesta.json();
-  }
-
-  const refrescarProductos = async () => {
-    let respuesta = await getProductos();
-
-    setProductos(respuesta);
-  }
-
-  useEffect(() => {
-    refrescarProductos();
-  }, [])
-
-
-
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+  const token = getToken();
 
   return (
-    // <>
-    //   <Container maxWidth="sm">
-    //     <Welcome message="ASW students"/>
-    //     <Box component="div" sx={{ py: 2}}>This is a basic example of a React application using Typescript. You can add your email to the list filling the form below.</Box>
-    //     <EmailForm OnUserListChange={refreshUserList}/>
-    //     <UserList users={users}/>
-    //     <Link href="https://github.com/pglez82/asw2122_0">Source code</Link>
-    //   </Container>
-    // </>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/carrito" element={<Carrito productos={productos}/>}/>
-          <Route path="/productos" element={<Catalogo/>} />
-          <Route path="/login" element={<Login/>} />
-        </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path='login' element={<Login />} />
+        {/* <Route path="/carrito" element={<Carrito productos={productos} />} /> */}
+        <Route path="productos" element={<Catalogo />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- webapp/src/pages/login.tsx
+++ webapp/src/pages/login.tsx
@@ -1,14 +1,28 @@
-import React, {BaseSyntheticEvent, useState } from 'react'
-import {Button, Checkbox, FormControlLabel, TextField, Typography } from '@mui/material'
+import React, { BaseSyntheticEvent, useEffect, useState } from 'react'
+import { Button, Checkbox, FormControlLabel, TextField, Typography } from '@mui/material'
 import './login.css'
+import { useNavigate } from 'react-router-dom'
+import { isLogeado } from '../App'
+
+function setToken(userToken: any) {
+    sessionStorage.setItem('token', JSON.stringify(userToken));
+}
 
 const Login = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (isLogeado())
+            navigate("/productos")
+    }, [])
 
     const [tipo, setTipo] = useState('password')
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [validationUsername, setValidationUsername] = useState('');
     const [validationPassword, setValidationPassword] = useState('');
+
+
 
     /**
      * Permite mostrar la contraseña
@@ -20,8 +34,8 @@ const Login = () => {
             setTipo("password")
     }
 
-    const handleInput = (e : BaseSyntheticEvent) => {
-        switch(e.target.name){
+    const handleInput = (e: BaseSyntheticEvent) => {
+        switch (e.target.name) {
             case "username":
                 setUsername(e.target.value);
                 break;
@@ -31,28 +45,39 @@ const Login = () => {
         }
     }
 
-    const comprobarDatos = (e:BaseSyntheticEvent) : boolean => {
-       
+    const handleSumbit = async (e: BaseSyntheticEvent) => {
+        comprobarDatos(e);
+
+        // TODO
+        // llamada al backend
+        if (password === '12345') {
+            const token = "token123"
+            if (token != null) {
+                setToken(token);
+                navigate("/productos");
+            }
+        }
+    };
+
+    const comprobarDatos = (e: BaseSyntheticEvent): void => {
         if (username.length <= 0)
             setValidationUsername("Name too short");
         if (password.length <= 0)
             setValidationPassword("Password too short");
-        
-        return false;
     }
 
     return (
         <div>
-            <Typography  variant='h2' gutterBottom>Inicie sesión</Typography>
-                <form onSubmit={comprobarDatos} style={{display: 'flex', flexDirection: 'column'}}>
-                    <TextField label="Usuario" name='username' onChange={handleInput} helperText={validationUsername} variant="outlined" margin="dense"></TextField>
-                    <TextField type={tipo} name="password" onChange={handleInput} helperText={validationPassword} label="Contraseña" variant="outlined" margin="dense"></TextField>
-                    <div style={{ display: 'flex', justifyContent: 'space-between', margin: '0.5em' }}>
-                        <FormControlLabel control={<Checkbox onChange={() => cambiarVisibilidad()}/>} label="Mostrar" />
-                        <Button type='submit' variant="contained">Iniciar sesión</Button>  
-                    </div>
-                </form>
-            <Typography style={{justifyContent: 'end'}}  variant='h6' gutterBottom>¿Sin cuenta? Regístrate <a href='/signup'>aquí</a></Typography>
+            <Typography variant='h2' gutterBottom>Inicie sesión</Typography>
+            <form onSubmit={handleSumbit} style={{ display: 'flex', flexDirection: 'column' }}>
+                <TextField label="Usuario" name='username' onChange={handleInput} helperText={validationUsername} variant="outlined" margin="dense" autoComplete="true"></TextField>
+                <TextField type={tipo} name="password" onChange={handleInput} helperText={validationPassword} label="Contraseña" variant="outlined" margin="dense" autoComplete="true"></TextField>
+                <div style={{ display: 'flex', justifyContent: 'space-between', margin: '0.5em' }}>
+                    <FormControlLabel control={<Checkbox onChange={() => cambiarVisibilidad()} />} label="Mostrar" />
+                    <Button type='submit' variant="contained">Iniciar sesión</Button>
+                </div>
+            </form>
+            <Typography style={{ justifyContent: 'end' }} variant='h6' gutterBottom>¿Sin cuenta? Regístrate <a href='/signup'>aquí</a></Typography>
         </div>
     )
 }
