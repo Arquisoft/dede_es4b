e97a398ce6d51f423e3fbd0847b443306aa7a6db
UO276213 <uo276213@uniovi.es>
2022-04-10 15:16:49 +0200

    Catálogo a tailwind

--- webapp/src/components/ProductosCatalogo/ProductosCatalogo.tsx
+++ webapp/src/components/ProductosCatalogo/ProductosCatalogo.tsx
@@ -1,74 +1,39 @@
-import React, {useState} from 'react';
-import {Grid} from '@material-ui/core';
+import React, {useEffect, useState } from 'react';
 import ProductoCatalogo from './ProductoCatalogo/ProductoCatalogo';
-import useStyles from './styles';
-import {Button, Divider, Menu, MenuItem, Pagination} from "@mui/material";
-import { Icon } from '@iconify/react';
-import Box from "@mui/material/Box";
 import { Producto } from '../../shared/shareddtypes';
+import MenuOrdenarProductos from './MenuOrdenarProductos/MenuOrdenarProductos';
+import Paginacion from '../Paginacion/Paginacion';
 
-const ProductosCatalogo = ({productos} : {productos : Producto[]}) => {
 
 
-    // Ordenar
-    const [open, setOpen] = useState(null);
+const ProductosCatalogo = ({ productos }: { productos: Producto[] }) => {
 
-    const handleOpen = (event : any) => {
-        setOpen(event.currentTarget);
-    };
+    const [actualizado, setActualizado] = useState(false);
 
-    const handleClose = () => {
-       // TODO
-        setOpen(null)
-    };
+    useEffect(() => {
+        setActualizado(true);
+    }, [actualizado])
 
-    const ordenarAsc = (lista : Producto[]) => {
-        lista.sort((a, b) => a.price - b.price);
-        handleClose();
-    }
-    
-    const ordenarDesc = (lista : Producto[])  => {
-        lista.sort((a, b) => - a.price + b.price);
-        handleClose();
-    }
 
-    const classes = useStyles();
     return (
-        <>
-            <main className={classes.main}>
-                <div className={classes.titulo}>
-                    <Box component="h1" color="indigo" >Catálogo de productos</Box>
-                    <Divider />
-                </div>
+        <div className='grid'>
 
-                <div className={classes.filtro}>
-                    <Button onClick={handleOpen} disableRipple className={classes.paginacion}>
-                        Ordenar
-                        <Icon icon="ic:round-filter-list" />
-                    </Button>
-                </div>
+            <h2 className="text-3xl font-extrabold tracking-tight text-gray-900">Catálogo de productos</h2>
 
-                <Menu
-                    keepMounted
-                    anchorEl={open}
-                    open={Boolean(open)}
-                    onClose={handleClose}
-                    anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-                    transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-                >
-                    <MenuItem onClick={() => ordenarAsc(productos)}>Precio ascendente</MenuItem>
-                    <MenuItem onClick={() => ordenarDesc(productos)}>Precio descendente</MenuItem>
-                </Menu>
-                <Grid container justifyContent="center" spacing={1}>
-                    {productos.map((producto : any) => (
-                        <Grid item key={producto.ids} xs={12} sm={6} md={4} lg={3}>
-                            <ProductoCatalogo producto={producto} />
-                        </Grid>
-                    ))}
-                </Grid>
-                <Pagination className={classes.paginacion} count={10} shape="rounded" />
-            </main>
-        </>
+            <div className="order-2 mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+                {productos.map((producto) => (
+                    <ProductoCatalogo key={producto._id} producto={producto} />
+                ))}
+            </div>
+
+            <div className='flex justify-end order-1'>
+                <MenuOrdenarProductos productos={productos} setActualizado={setActualizado} />
+            </div>
+
+            <div className='order-3'>
+                <Paginacion />
+            </div>
+        </div>
     );
 }
 
