be81be6b3656556a473df76ad2b51f3224001db6
uo277553 <uo277553@uniovi.es>
2022-04-24 13:24:48 +0200

    Arreglo de problemas mergeo con dev

--- restapi/controladores/LoginController.ts
+++ restapi/controladores/LoginController.ts
@@ -7,40 +7,40 @@ const User = require('../models/user')
 //funciones
 const login = async (req: Request, res: Response) => {
 
-    const user = await User.findOne({'userName': req.body.userName})
+  const user = await User.findOne({'userName': req.body.userName})
 
-    if(!user){
+  if(!user){
 
+    res.status(401).json({ error: "invalid user or password" })
+
+  } else {
+
+    const samePassword = await bcrypt.compare(req.body.password, user.password)
+
+    if(!samePassword){
       res.status(401).json({ error: "invalid user or password" })
-
     } else {
 
-      const samePassword = await bcrypt.compare(req.body.password, user.password)
-
-      if(!samePassword){
-        res.status(401).json({ error: "invalid user or password" })
-      } else {
-
-        const userToken = {
-          id: user._id,
-          userName: req.body.userName,
-          role: user.role
-        }
-
-        const tokenSecret = process.env.SECRET
-
-        const token = jwt.sign(userToken, tokenSecret)
-
-        res.status(200).json({
-          userName: user.userName,
-          name: user.Name,
-          token
-        })
-
+      const userToken = {
+        id: user._id,
+        userName: req.body.userName,
+        role: user.role
       }
 
+      const tokenSecret = process.env.SECRET
+
+      const token = jwt.sign(userToken, tokenSecret)
+
+      res.status(200).json({
+        userName: user.userName,
+        name: user.Name,
+        token
+      })
+
     }
-  
+
+  }
+
 }
 
 module.exports = {
