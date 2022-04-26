5499398139aebb6249f789a0f9c1370d3100af2c
UO276213 <uo276213@uniovi.es>
2022-04-26 12:17:25 +0200

    Refactorización codigo del carrito

--- webapp/src/shared/shareddtypes.ts
+++ webapp/src/shared/shareddtypes.ts
@@ -1,15 +1,21 @@
-  export type Producto = {
-      _id: string
-      name:string;
-      price:number;
-      short_description:string;
-      long_description:string;
-      brand:string;
-      category:string;
-      sub_category:string;
-      image:string; 
-  
-      type:string;
-      color:string;
-      size:number;   
-  }
+export type Producto = {
+  _id: string
+  name: string;
+  price: number;
+  short_description: string;
+  long_description: string;
+  brand: string;
+  category: string;
+  sub_category: string;
+  image: string;
+
+  type: string;
+  color: string;
+  size: number;
+}
+
+export type ProductoCarrito = {
+  producto: Producto;
+  cantidad: number;
+  precioTotal: number;
+}
